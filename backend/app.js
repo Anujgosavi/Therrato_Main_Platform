541fb0ae0be117ab59a5121d9ad9e76230fd320a
Anuj Gosavi <anujgosavi2005@gmail.com>
2025-07-01 00:02:41 +0530

    fixed the cors issue

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3001", "https://therrato-main.onrender.com"], // add your frontend and backend URLs here
+    origin: "*", // add your frontend and backend URLs here
     credentials: true,
   })
 );
